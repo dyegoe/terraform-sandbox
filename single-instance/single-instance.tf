06ea6e7712e8e2b5d6096aaf326a8c6e8f3f6a38
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-08-17 22:51:29 +0300

    change instance type

--- single-instance/single-instance.tf
+++ single-instance/single-instance.tf
@@ -7,6 +7,6 @@ resource "aws_instance" "example" {
     ami = "ami-d834aba1"
     availability_zone = "eu-west-1a"
     key_name = "dyego-default"
-    instance_type = "t2.micro"
+    instance_type = "t2.nano"
     security_groups = [ "open-ssh" ]
 }
