d01507a81a31bc7fbfe3f6b942139a899bc98720
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-08-17 22:35:26 +0300

    add git ignore e first tf

---
+++ single-instance/single-instance.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    profile = "ansible"
+    region = "eu-west-1"
+}
+
+resource "aws_instance" "example" {
+    ami = "ami-d834aba1"
+    availability_zone = "eu-west-1a"
+    key_name = "dyego-default"
+    instance_type = "t2.micro"
+    security_groups = [ "open-ssh" ]
+}
