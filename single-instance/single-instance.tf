3f4b5b1b0e6acc8d83d342c1eeeabb8975a75311
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-08-20 13:47:39 +0300

    include s3 bucket with explicit dep

--- single-instance/single-instance.tf
+++ single-instance/single-instance.tf
@@ -1,16 +1,19 @@
 provider "aws" {
-    profile = "ansible"
-    region = "eu-west-1"
+  profile = "dyego"
+  region = "eu-west-1"
 }
-
+resource "aws_s3_bucket" "example" {
+  bucket = "terraform-sandbox-uoro5aex"
+  acl = "private"
+}
 resource "aws_instance" "example" {
-    ami = "ami-d834aba1"
-    availability_zone = "eu-west-1a"
-    key_name = "dyego-default"
-    instance_type = "t2.nano"
-    security_groups = [ "open-ssh" ]
+  ami = "ami-d834aba1"
+  availability_zone = "eu-west-1a"
+  key_name = "dyego-default"
+  instance_type = "t2.nano"
+  security_groups = [ "open-ssh" ]
+  depends_on = ["aws_s3_bucket.example"]
 }
-
 resource "aws_eip" "ip" {
   instance = "${aws_instance.example.id}"
 }
