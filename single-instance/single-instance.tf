b3e288844a804109026a4954c663865a106f8eac
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-08-20 17:09:31 +0300

    add python that create ami map and include it as var

--- single-instance/single-instance.tf
+++ single-instance/single-instance.tf
@@ -1,18 +1,16 @@
 provider "aws" {
-  profile = "dyego"
-  region = "eu-west-1"
-}
-resource "aws_s3_bucket" "example" {
-  bucket = "terraform-sandbox-uoro5aex"
-  acl = "private"
+  profile = "${var.profile}"
+  region = "${var.region}"
 }
 resource "aws_instance" "example" {
-  ami = "ami-d834aba1"
-  availability_zone = "eu-west-1a"
+  ami = "${lookup(var.amis, var.region)}"
+  availability_zone = "${var.region}a"
   key_name = "dyego-default"
   instance_type = "t2.nano"
   security_groups = [ "open-ssh" ]
-  depends_on = ["aws_s3_bucket.example"]
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
 }
 resource "aws_eip" "ip" {
   instance = "${aws_instance.example.id}"
